d24311b9037a6a44af567fa322388bded357e7a6
sai8151 <saikiranreddy8151@gmail.com>
2024-07-07 22:22:16 +0530

    #1

---
+++ prog1.cpp
@@ -0,0 +1,10 @@
+// 3. Divide the given array into two arrays such that both arrays have equal averages
+//         Sample input : [2 4 8 10 16] Sample output : [8] and [2 4 10 16]
+//         Sample input : [2 4 8 10]    Sample output : [4 8] and [2 10]
+#include <iostream>
+int main()
+{
+    int arr[10];
+
+    return 0;
+}
