9f52c6713d603536dbcc75ab7c0a64bf50ec2ce6
sai8151 <saikiranreddy8151@gmail.com>
2024-07-08 23:17:51 +0530

    #4

---
+++ p2.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <cstring>
+
+void printWordPattern(const std::string &word)
+{
+    int n = word.size();
+    char matrix[n][n];
+
+    // Initialize the matrix with spaces
+    for (int i = 0; i < n; ++i)
+    {
+        for (int j = 0; j < n; ++j)
+        {
+            matrix[i][j] = ' ';
+        }
+    }
+
+    // Populate the matrix with characters of the word
+    for (int i = 0; i < n; ++i)
+    {
+        matrix[i][i] = word[i];
+        matrix[i][n - i - 1] = word[i];
+    }
+
+    // Print the matrix
+    for (int i = 0; i < n; ++i)
+    {
+        for (int j = 0; j < n; ++j)
+        {
+            std::cout << matrix[i][j];
+        }
+        std::cout << std::endl;
+    }
+}
+
+int main()
+{
+    std::string word = "PROGRAM";
+    printWordPattern(word);
+    return 0;
+}
